bfcebad93b903cfc96102c80ff054c561e25a962
Andrew Nikitin <143314594+AxolotlMonokle@users.noreply.github.com>
2025-11-01 13:24:41 +0200

    day3

--- main.js
+++ main.js
@@ -10,16 +10,54 @@ async function getProducts() {
 
 function getCardHTML(product) {
     return `<div class="card" style="width: 18rem;">
-  <img src="img/${product.img}" class="card-img-top" alt="...">
+  <img src="${product.image}" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">${product.title}</h5>
     <p class="card-price">Ціна: ${product.price} UAH</p>
-    <a href="#" class="btn btn-primary">Додати в кошик</a>
+    <a href="#" class="btn btn-primary" data-product='${JSON.stringify(product)}'>Додати в кошик</a>
   </div>
 </div>`;
 }
 
+class ShoppingCart {
+    constructor() {
+        this.items = {} // об’єкт з товарами у кошику
+        this.total = 0  // загальна вартість замовлення
+    }
 
+    addItem(item) { 
+        // Додавання товару до кошика 
+        if (this.items[item.title]) {
+            this.items[item.title].quantity += 1
+        } else {this.items[item.title] = item
+            this.items[item.title].quantity = 1
+        }
+        this.saveCartToCookies();
+    }  
+    	
+    saveCartToCookies() {
+         // збереження кошика у кукі
+        let cartJSON = JSON.stringify(this.items);
+        document.cookie = `cart=${cartJSON}; max-age=${60 * 60 * 24 * 7}; path=/`;
+    }
+
+    loadCartFromCookies() { 
+        // Завантаження кошика з кукі
+        let cartCookie = getCookieValue('cart');
+        if (cartCookie && cartCookie !== '') {
+        this.items = JSON.parse(cartCookie);
+    }
+
+    }
+}
+let cart = new ShoppingCart() // Створення об'єкта кошика
+
+function addToCart(event) {
+    const productData = event.target.getAttribute('data-product');
+    const product = JSON.parse(productData);
+    cart.addItem(product);
+
+}
 
 // Викликаємо асинхронну функцію та очікуємо на отримання продуктів
 getProducts().then(function (products) {
